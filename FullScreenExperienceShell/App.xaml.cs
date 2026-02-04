5090ee2f48b2804cd62f004c7939eb837e79f3f0
driver1998 <driver1998.ms@outlook.com>
2026-02-05 00:52:26 +0800

    Add FSE Shell declaration

--- FullScreenExperienceShell/App.xaml.cs
+++ FullScreenExperienceShell/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,10 +7,20 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
+using Microsoft.Win32.SafeHandles;
+using Microsoft.Windows.AppLifecycle;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Win32;
+using Windows.Win32.Foundation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,6 +33,7 @@ namespace FullScreenExperienceShell
     public partial class App : Application
     {
         private Window? _window;
+        private static SafeFileHandle? redirectEventHandle = null;
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
@@ -45,6 +52,71 @@ namespace FullScreenExperienceShell
         {
             _window = new MainWindow();
             _window.Activate();
+        }
+
+        [STAThread]
+        static int Main(string[] args)
+        {
+            bool isRedirect = DecideRedirection();
+
+            if (!isRedirect)
+            {
+                Application.Start((p) =>
+                {
+                    var context = new DispatcherQueueSynchronizationContext(
+                        DispatcherQueue.GetForCurrentThread());
+                    SynchronizationContext.SetSynchronizationContext(context);
+                    _ = new App();
+                });
+            }
+
+            return 0;
+        }
+
+        private static bool DecideRedirection()
+        {
+            bool isRedirect = false;
+            AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+            ExtendedActivationKind kind = args.Kind;
+            AppInstance keyInstance = AppInstance.FindOrRegisterForKey("MySingleInstanceApp");
+
+            if (keyInstance.IsCurrent)
+            {
+                keyInstance.Activated += OnActivated;
+            }
+            else
+            {
+                isRedirect = true;
+                RedirectActivationTo(args, keyInstance);
+            }
+
+            return isRedirect;
+        }
+
+        private static void OnActivated(object? sender, AppActivationArguments args)
+        {
+            ExtendedActivationKind kind = args.Kind;
+        }
+
+        private static void RedirectActivationTo(AppActivationArguments args,
+                                                 AppInstance keyInstance)
+        {
+            redirectEventHandle = PInvoke.CreateEvent(null, true, false, null);
+            Task.Run(() =>
+            {
+                keyInstance.RedirectActivationToAsync(args).AsTask().Wait();
+                PInvoke.SetEvent(redirectEventHandle);
+            });
+
+            uint CWMO_DEFAULT = 0;
+            uint INFINITE = 0xFFFFFFFF;
+            _ = PInvoke.CoWaitForMultipleObjects(
+               CWMO_DEFAULT, INFINITE,
+               [(HANDLE)redirectEventHandle.DangerousGetHandle()], out uint handleIndex);
+
+            // Bring the window to the foreground
+            Process process = Process.GetProcessById((int)keyInstance.ProcessId);
+            PInvoke.SetForegroundWindow(new HWND(process.MainWindowHandle));
         }
     }
 }
